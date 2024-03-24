0b644bc8652acbbde7c94d92c1010a4620ef094f
JeremyDwayne <jeremy@jeremydwayne.com>
2024-03-23 23:33:32 -0400

    💪 fix svg tags for react

--- src/app/_components/svgIcons.tsx
+++ src/app/_components/svgIcons.tsx
@@ -6,9 +6,9 @@ export function Checkmark() {
       className="h-[18px] w-[18px] shrink-0 fill-success opacity-80"
     >
       <path
-        fill-rule="evenodd"
+        fillRule="evenodd"
         d="M16.704 4.153a.75.75 0 01.143 1.052l-8 10.5a.75.75 0 01-1.127.075l-4.5-4.5a.75.75 0 011.06-1.06l3.894 3.893 7.48-9.817a.75.75 0 011.05-.143z"
-        clip-rule="evenodd"
+        clipRule="evenodd"
       ></path>
     </svg>
   );
@@ -32,9 +32,9 @@ export function PaperAirplane() {
       xmlns="http://www.w3.org/2000/svg"
       viewBox="0 0 24 24"
       stroke="currentColor"
-      stroke-width="2"
-      stroke-linecap="round"
-      stroke-linejoin="round"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
       className="h-[18px] w-[18px] fill-white/10 transition-transform duration-200 group-hover:translate-x-0.5 group-hover:fill-white/20"
     >
       <path d="m3 3 3 9-3 9 19-9Z"></path>
