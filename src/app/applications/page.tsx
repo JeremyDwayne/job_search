4e574985b278608571314da76b4a158ebb759891
JeremyDwayne <jeremy@jeremydwayne.com>
2024-05-03 15:01:50 -0500

    pull applications from db

--- src/app/applications/page.tsx
+++ src/app/applications/page.tsx
@@ -1,11 +1,27 @@
 import CreateApplicationForm from "./CreateApplicationForm";
+import { getJobApplications } from "~/server/queries";
 
-export default function ApplicationsPage() {
+export default async function ApplicationsPage() {
+  const applications = await getJobApplications();
+
   return (
     <div className="mt-0 pt-0">
       <h1 className="text-2xl">Applications</h1>
       <hr />
       <CreateApplicationForm />
+      <hr />
+      {applications.map((application) => (
+        <div key={application.id}>
+          <span>{application.title}</span>
+          <span>{application.company}</span>
+          <span>
+            <a href={application.job_description_url ?? "#"}>Job Description</a>
+          </span>
+          <span>
+            {application.salary_low}-{application.salary_high}
+          </span>
+        </div>
+      ))}
     </div>
   );
 }
